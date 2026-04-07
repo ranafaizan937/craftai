b0fdb1c3144848918df3dea02bc9c953637f1d1a
webrootlimited <103070823+ranafaizan937@users.noreply.github.com>
2026-04-07 20:38:02 +0500

    removed

--- src/landingpage/Testing.tsx
+++ src/landingpage/Testing.tsx
@@ -4,7 +4,6 @@ const Testing = () => {
   return (
     <div>Testing page addedd
       i have added a one new thing lorem10hjgjkh
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit quo nemo rerum ad voluptatibus possimus esse earum atque, adipisci sequi?
     </div>
   )
 }
