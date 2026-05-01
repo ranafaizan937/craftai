722d1a59d7aaae6ca3c91900a956b9d45dfff8a4
webrootlimited <103070823+ranafaizan937@users.noreply.github.com>
2026-05-01 19:47:33 +0500

    removed

--- src/landingpage/Testing.tsx
+++ src/landingpage/Testing.tsx
@@ -3,10 +3,7 @@ import React from 'react'
 const Testing = () => {
   return (
     <div>pta ni kyha talkeef he is sajan ko
-      <div>
-        added a new div
-        hehehehehhehehehehehehheh
-      </div>
+      
     </div>
   )
 }
