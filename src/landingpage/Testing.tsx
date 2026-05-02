b59c97c8b777db4ade3b9d65f3a53a5f40821061
webrootlimited <103070823+ranafaizan937@users.noreply.github.com>
2026-05-02 21:25:31 +0500

    added

--- src/landingpage/Testing.tsx
+++ src/landingpage/Testing.tsx
@@ -3,7 +3,11 @@ import React from 'react'
 const Testing = () => {
   return (
     <div>pta ni kyha talkeef he is sajan ko
-      
+      <div>
+        new div add krdi he
+        <p>This is a paragraph inside the new div</p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quae eius sit ex ipsam, consectetur ut?
+      </div>
     </div>
   )
 }
