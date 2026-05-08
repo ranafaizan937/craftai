d63f6331ced08421b50a2c5e3a0151eefc3ef76b
webrootlimited <103070823+ranafaizan937@users.noreply.github.com>
2026-05-08 21:11:12 +0500

    removed

--- src/landingpage/farig.jsx
+++ src/landingpage/farig.jsx
@@ -5,25 +5,7 @@ const farig = () => {
     <div>
       {" "}
       hun farig bndy nu ki kawa
-      <div>
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eaque fugit
-        dolor ea, aliquid necessitatibus architecto vitae amet voluptatem
-        debitis. Repellat consectetur sint dolor rerum ducimus quis facilis! Ab
-        doloribus provident voluptate optio quae, dolore natus error. Non ut
-        optio placeat laborum nam laudantium, quisquam voluptatem fuga rem
-        alias? Mollitia repellat voluptates exercitationem inventore vel,
-        aliquid assumenda. Ipsam omnis quo minus, eligendi cum vel possimus
-        aspernatur quisquam ex ullam tempora aut illo pariatur ad eveniet
-        cupiditate. Adipisci dolores soluta quasi eius. Facere, numquam. Dicta
-        possimus doloribus non minima, molestias consequuntur consequatur animi
-        debitis fugiat nemo soluta, delectus earum facere et pariatur. Sequi et
-        iure sed optio ex quo explicabo, eaque laboriosam alias excepturi minus!
-        At nobis tempora asperiores aperiam nesciunt magnam eveniet possimus,
-        exercitationem delectus. Laborum, inventore. Minus, perferendis eligendi
-        nulla in atque magnam sequi, obcaecati neque magni labore quas quae
-        dolorem repellat odit. Officiis dolores veniam repellat quidem? Itaque,
-        perspiciatis.
-      </div>
+      
     </div>
   );
 };
