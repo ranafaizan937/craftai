1839e4465ba89f965fb5556d26098b4daabddac3
webrootlimited <103070823+ranafaizan937@users.noreply.github.com>
2026-05-09 15:34:01 +0500

    added

--- src/landingpage/farig.jsx
+++ src/landingpage/farig.jsx
@@ -5,7 +5,9 @@ const farig = () => {
     <div>
       {" "}
       hun farig bndy nu ki kawa
-      
+      <div>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repellendus id ex corrupti placeat cumque reiciendis sapiente culpa itaque, exercitationem aliquam earum facilis cupiditate alias sed ipsum in hic consequuntur eum. Quam excepturi accusamus ducimus! Ipsam dolore a praesentium culpa quas!
+      </div>
     </div>
   );
 };
